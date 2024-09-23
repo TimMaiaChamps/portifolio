4181a26bf3e9fec7935f72025787227921eaf65d
Tim Maia Champs <luizhenriquecserpa@gmail.com>
2024-09-23 17:26:21 -0300

    att gggg

---
+++ index.js
@@ -0,0 +1,11 @@
+let footer = document.getElementById("footer")
+let h4 = document.createElement('h4')
+h4.textContent = 'luizhenriquecserpa@gmail.com'
+h4.style.fontFamily = 'fontP'
+
+footer.appendChild(h4)
+
+footer.style.display = 'flex'
+footer.style.flexDirection = 'row-reverse'
+footer.style.alignItems = 'center'
+footer.style.justifyContent = 'space-between'
