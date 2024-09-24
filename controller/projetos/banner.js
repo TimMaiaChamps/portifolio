77c5c1ecebe6bc463886e2d59ec8e192acb80fff
Tim Maia Champs <luizhenriquecserpa@gmail.com>
2024-09-24 16:56:04 -0300

    att 24/09

---
+++ controller/projetos/banner.js
@@ -0,0 +1,10 @@
+let banners = document.getElementById('banners')
+
+
+export function trocarBanner(){
+
+banners.style.left = '-' + numeroBanner + '00vw'
+
+
+
+}
---
+++ controller/projetos/projetos.js
@@ -0,0 +1,5 @@
+import { trocarBanner } from "./banner";    
+
+window.trocarBanner = trocarBanner
+
+document.addEventListener("DOMContentLoaded", function(){})
